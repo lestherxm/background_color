e6d331ffa1cf7d7578b8b52aa52ee7fb6d12cf22
lestherxm <lestherxm@gmail.com>
2023-02-05 19:49:55 -0600

    adding more colors and audio

--- src/script.js
+++ src/script.js
@@ -1,10 +1,41 @@
 const background = document.querySelector('.bgcolor-container');
 const btnColorChanger = document.querySelector('#btnColorChanger');
-const colors = ['red', 'green', 'blue', 'yellow']
+const audio = new Audio('../src/audio/action11.mp3');
+audio.volume = 0.5;
+const colors = [
+    "aliceblue",
+    "antiquewhite",
+    "aqua",
+    "aquamarine",
+    "azure",
+    "beige",
+    "bisque",
+    "black",
+    "blanchedalmond",
+    "blue",
+    "blueviolet",
+    "brown",
+    "burlywood",
+    "cadetblue",
+    "chartreuse",
+    "chocolate",
+    "coral",
+    "cornflowerblue",
+    "cornsilk",
+    "crimson",
+    "cyan",
+    "darkblue",
+    "darkcyan",
+    "darkgoldenrod",
+    "darkgray",
+    "darkgrey"
+];
 let random = 0;
 let previous = 0;
 
+
 btnColorChanger.addEventListener('click', () => {
+    audio.play();
     do {
         random = Math.floor(Math.random() * (colors.length - 1)) + 1;
     } while (random === previous);
