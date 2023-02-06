56f12581e1cb14666c71f26e50db20841ad686f3
lestherxm <lestherxm@gmail.com>
2023-02-05 20:06:41 -0600

    fixing bug with the audio, know using Web Audio API

--- src/script.js
+++ src/script.js
@@ -1,7 +1,5 @@
 const background = document.querySelector('.bgcolor-container');
 const btnColorChanger = document.querySelector('#btnColorChanger');
-const audio = new Audio('../src/audio/action11.mp3');
-audio.volume = 0.5;
 const colors = [
     "aliceblue",
     "antiquewhite",
@@ -33,12 +31,27 @@ const colors = [
 let random = 0;
 let previous = 0;
 
-
 btnColorChanger.addEventListener('click', () => {
-    audio.play();
+    beepSound();
     do {
         random = Math.floor(Math.random() * (colors.length - 1)) + 1;
     } while (random === previous);
     background.style.backgroundColor = colors[random];
     previous = random;
+    
 });
+
+/*
+ * The AudioContext interface in the Web Audio API to generate simple sounds without the need for a pre-recorded sound file.
+ */
+
+function beepSound(){
+    // This code basically makes possible the @beep when you click the button to change color.
+    const context = new (window.AudioContext || window.webkitAudioContext)();
+    const oscillator = context.createOscillator();
+    oscillator.type = "sine";
+    oscillator.frequency.value = 130;
+    oscillator.connect(context.destination);
+    oscillator.start();
+    oscillator.stop(context.currentTime + 0.1);
+}
